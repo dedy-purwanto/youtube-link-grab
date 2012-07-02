d8ffedcccad9b83f7687ac27e87a9770db969fb9
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-02 15:17:02 +0800

    Use normal scrapping mechanism

--- grab.py
+++ grab.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python2.7
 
 import requests
-import json
 import sys
+from bs4 import BeautifulSoup
 
-def grab(keyword, limit, author, start=1):
-    max_limit = limit if limit <= 50 else 50
-    
-    url = (
-            "http://gdata.youtube.com/feeds/base/videos?max-results=%s&alt=json&q=%s&author=%s&orderby=published&start-index=%s" %
-            (max_limit, keyword, author, start)
-    )
+def grab(keyword, author, page=1):
+    url = ("http://www.youtube.com/user/%s/videos?sort=da&view=0&page=%s" 
+            % (author, page))
+    sys.stdout.write("#Processing %s\r\n" % url)
+    soup = BeautifulSoup(requests.get(url).content)
+    videos_container = soup.find("ol", {"class": "channel-videos-list"})
+    videos = videos_container.find_all("li", {"class": "yt-c3-grid-item"})
 
-    output = json.loads(requests.get(url).content)['feed']
-    for entry in output['entry']:
-        title = entry['title']['$t']
-        link = entry['link'][0]['href']
-        date = entry['published']['$t']
+    if len(videos) > 0:
+        for video in videos:
+            detail = (video.find("h3", {"class":"yt-c3-grid-item-title"})
+                        .find("a"))
+            if detail.contents.__str__().find(keyword) >= 0:
+                title = detail.contents[0].strip()
+                url = detail['href']
+                sys.stdout.write("%s#%s\r\n" % (url, title))
 
-        sys.stdout.write("%s#%s#%s\r\n" % (link, date, title))
+    if len(videos_container.contents.__str__()) > 10:
+        grab(keyword, author, page+1)
 
-    if limit - 50 > start:
-        grab(keyword, limit, author, start+50)
-
-
-grab("ovj", 100, "gerandong24")
+grab("Opera Van Java", "gerandong24")
