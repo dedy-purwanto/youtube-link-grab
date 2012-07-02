a16d962a36ea448646802e59bdbcc38ae78b41af
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-02 16:01:28 +0800

    Add retry mechanism

--- grab.py
+++ grab.py
@@ -10,22 +10,27 @@ def grab(keyword, author, out, page=1):
     sys.stdout.write("#Processing %s\r\n" % url)
     soup = BeautifulSoup(requests.get(url).content)
     videos_container = soup.find("ol", {"class": "channel-videos-list"})
-    videos = videos_container.find_all("li", {"class": "yt-c3-grid-item"})
 
-    if len(videos) > 0:
-        count = 0
-        for video in videos:
-            detail = (video.find("h3", {"class":"yt-c3-grid-item-title"})
-                        .find("a"))
-            if detail.contents.__str__().find(keyword) >= 0:
-                title = detail.contents[0].strip()
-                url = detail['href']
-                out.write("%s#%s\r\n" % (url, title))
-                count += 1
-        print "%s video links grabbed\r\n" % count
+    try:
+        videos = videos_container.find_all("li", {"class": "yt-c3-grid-item"})
+    except AttributeError:
+        sys.stdout.write("#Retrying..\r\n")
+        grab(keyword, author, out, page)
+    else:
+        if len(videos) > 0:
+            count = 0
+            for video in videos:
+                detail = (video.find("h3", {"class":"yt-c3-grid-item-title"})
+                            .find("a"))
+                if detail.contents.__str__().find(keyword) >= 0:
+                    title = detail.contents[0].strip()
+                    url = detail['href']
+                    out.write("%s#%s\r\n" % (url, title))
+                    count += 1
+            print "%s video links grabbed\r\n" % count
 
-    if len(videos_container.contents.__str__()) > 10:
-        grab(keyword, author, out, page=page+1)
+        if len(videos_container.contents.__str__()) > 10:
+            grab(keyword, author, out, page=page+1)
 
 output = open("/tmp/videos.txt", "w")
 grab("Opera Van Java", "gerandong24", out=output)
