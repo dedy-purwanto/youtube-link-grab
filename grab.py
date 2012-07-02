32463f398aa6a7fb162bca552a18ecd8a62b2e76
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-02 16:22:59 +0800

    Refining some small stuff

--- grab.py
+++ grab.py
@@ -7,14 +7,14 @@ from bs4 import BeautifulSoup
 def grab(keyword, author, out, page=1):
     url = ("http://www.youtube.com/user/%s/videos?sort=da&view=0&page=%s" 
             % (author, page))
-    sys.stdout.write("#Processing %s\r\n" % url)
+    sys.stdout.write("Processing %s..." % url)
     soup = BeautifulSoup(requests.get(url).content)
     videos_container = soup.find("ol", {"class": "channel-videos-list"})
 
     try:
         videos = videos_container.find_all("li", {"class": "yt-c3-grid-item"})
     except AttributeError:
-        sys.stdout.write("#Retrying..\r\n")
+        sys.stdout.write(" FAILED. Retrying..")
         grab(keyword, author, out, page)
     else:
         if len(videos) > 0:
@@ -27,10 +27,12 @@ def grab(keyword, author, out, page=1):
                     url = detail['href']
                     out.write("%s#%s\r\n" % (url, title))
                     count += 1
-            print "%s video links grabbed\r\n" % count
+            print " %s video links grabbed" % count
 
         if len(videos_container.contents.__str__()) > 10:
             grab(keyword, author, out, page=page+1)
+        else:
+            print " End of pages"
 
 output = open("/tmp/videos.txt", "w")
 grab("Opera Van Java", "gerandong24", out=output)
