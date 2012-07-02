e3deadcd9a85986b431859b4fb6cbfa8849bd4a5
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-02 15:25:09 +0800

    Save to fil

--- grab.py
+++ grab.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python
 
 import requests
 import sys
 from bs4 import BeautifulSoup
 
-def grab(keyword, author, page=1):
+def grab(keyword, author, out, page=1):
     url = ("http://www.youtube.com/user/%s/videos?sort=da&view=0&page=%s" 
             % (author, page))
     sys.stdout.write("#Processing %s\r\n" % url)
@@ -13,15 +13,19 @@ def grab(keyword, author, page=1):
     videos = videos_container.find_all("li", {"class": "yt-c3-grid-item"})
 
     if len(videos) > 0:
+        count = 0
         for video in videos:
             detail = (video.find("h3", {"class":"yt-c3-grid-item-title"})
                         .find("a"))
             if detail.contents.__str__().find(keyword) >= 0:
                 title = detail.contents[0].strip()
                 url = detail['href']
-                sys.stdout.write("%s#%s\r\n" % (url, title))
+                out.write("%s#%s\r\n" % (url, title))
+                count += 1
+        print "%s video links grabbed\r\n" % count
 
     if len(videos_container.contents.__str__()) > 10:
-        grab(keyword, author, page+1)
+        grab(keyword, author, out, page=page+1)
 
-grab("Opera Van Java", "gerandong24")
+output = open("/tmp/videos.txt", "w")
+grab("Opera Van Java", "gerandong24", out=output)
